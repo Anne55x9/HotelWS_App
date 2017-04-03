c0e3b683787d3723754662b47fa14555b87bef1c
anne55x9 <anne55x9@edu.easj.dk>
2017-04-03 17:51:16 +0200

    MvvM struktur implementeret med Webservices og gæstbookhandler samt relayCommand.

---
+++ HotelWS_App/ViewModel/GuestBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelWS_App.ViewModel
+{
+    class GuestBookViewModel
+    {
+    }
+}
