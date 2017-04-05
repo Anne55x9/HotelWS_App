41ef3281d80835f557125053df1a5418b538b2e3
anne55x9 <anne55x9@edu.easj.dk>
2017-04-05 09:47:43 +0200

    Installed json and WepAPi client nuggets.

--- HotelWS_App/Web/WebServices.cs
+++ HotelWS_App/Web/WebServices.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 
 
+
 namespace HotelWS_App.Web
 {
     public class WebServices
@@ -17,8 +18,41 @@ namespace HotelWS_App.Web
 
         const string serverUrl = "http://hotelappwebsevices20170403062059.azurewebsites.net/";
 
-       
-      
+        public static ObservableCollection<Guest> LoadEventsFromJsonAsync()
+        {
+            //HttpClientHandler handler = new HttpClientHandler();
+            //HttpClient client = new HttpClient();
+            //handler.UseDefaultCredentials = true;
+
+            ObservableCollection<Guest> Temp_list = new ObservableCollection<Guest>();
+
+            using (var client1 = new HttpClient())
+            {
+                client1.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client1.DefaultRequestHeaders.Clear();
+                client1.BaseAddress = new Uri(serverUrl);
+
+                try
+                {
+                    HttpResponseMessage response = client1.GetAsync("api/Guests").Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+
+
+                        //Temp_list = response.Content.ReadAsAsync<ObservableCollection<Guest>>().Result;
+                    }
+                }
+                catch (Exception)
+                {
+                    Temp_list = null;
+
+                }
+
+                return Temp_list;
+            }
+        }
+
 
     }
 }
